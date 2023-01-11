3a245187c1cbbe3f6405440776988287408855ad
kevinnail <fumalicious@gmail.com>
2023-01-11 12:36:34 -0800

    title variable set

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import './Editor.css';
 
-export default function Editor() {
+export default function Editor({ title, setTitle }) {
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
   return (
     <div className="editor">
       <div className="form-control">
-        <input name="title" type="text" />
+        <input name="title" type="text" value={title} onChange={handleChange} />
         <label htmlFor="title">Title</label>
       </div>
       <div className="form-control">
